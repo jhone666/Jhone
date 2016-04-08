66b94e672ca54758f3a667facd8e9c753ddb8f71
jhone666 <jhone163@163.com>
2016-04-08 19:56:57 +0800

    ..

---
+++ app/src/main/java/com/jhone/demo/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.jhone.demo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jhone.demo.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
